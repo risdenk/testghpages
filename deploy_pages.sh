fdb097c514ca04b1d0c9e4a6dd7e053545de8271
Kevin Risden <risdenk@avalonconsult.com>
2016-03-04 10:29:55 -0600

    Fix site

--- deploy_pages.sh
+++ deploy_pages.sh
@@ -9,11 +9,15 @@ rm -rf gh-pages || exit 0;
 git clone -b gh-pages --single-branch "https://${GH_TOKEN}@${GH_REF}" gh-pages
 
 rm -rf gh-pages/*
+shopt -s dotglob
 cp -r docs/* gh-pages/
 
 cd gh-pages
+
+echo "Updated gh-pages based on $TRAVIS_BRANCH commit $TRAVIS_COMMIT on $(date)" > gh-pages-version.html
+
 git config user.email "travisci@travisci.org"
 git config user.name "Travis CI"
 git add -A .
-git commit -m "Updates gh-pages based on $TRAVIS_BRANCH commit $TRAVIS_COMMIT" || exit 0
+git commit -m "Updated gh-pages based on $TRAVIS_BRANCH commit $TRAVIS_COMMIT" || exit 0
 git push --quiet origin gh-pages > /dev/null 2>&1
